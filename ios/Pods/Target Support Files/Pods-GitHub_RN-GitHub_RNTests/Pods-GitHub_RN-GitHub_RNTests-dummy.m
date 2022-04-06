9b8d5fd605184dd42ac80fdc3f1e1d6f2fee33de
jliu0105 <m15734125025@gmail.com>
2022-04-05 21:46:59 -0400

    github

---
+++ ios/Pods/Target Support Files/Pods-GitHub_RN-GitHub_RNTests/Pods-GitHub_RN-GitHub_RNTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GitHub_RN_GitHub_RNTests : NSObject
+@end
+@implementation PodsDummy_Pods_GitHub_RN_GitHub_RNTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-GitHub_RN/Pods-GitHub_RN-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GitHub_RN : NSObject
+@end
+@implementation PodsDummy_Pods_GitHub_RN
+@end
